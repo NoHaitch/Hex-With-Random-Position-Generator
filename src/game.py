ff46c12bcdaa9ea61eb89ec4f6102ab9a833abeb
Chiko but no H <franciscotrianto@gmail.com>
2023-12-07 23:49:40 +0700

    feat: move and fix check win to game.py

--- src/game.py
+++ src/game.py
@@ -1,23 +1,26 @@
 from display import *
-from typing import List
+from typing import List, Tuple
 
 def menu() -> int:
     while True:
         print("\n==================== ", end="");print_yellow("MENU");print(" ====================")
         print(" 1. Rules")
         print(" 2. Help")
-        print(" 3. Generate ")
+        print(" 3. Play ")
+        print(" 4. Generate ")
         print("Choose Command:")
         user_input = input_color("")
         
-        if user_input in ["1", "2", "3"]:
+        if user_input in ["1", "2", "3", "4"]:
             return int(user_input)
         elif user_input.lower() == "rules":
             return 1
         elif user_input.lower() == "help":
             return 2
-        elif user_input.lower() == "generate":
+        elif user_input.lower() == "play":
             return 3
+        elif user_input.lower() == "generate":
+            return 4
         else:
             print_red_nl("Invalid input\n")
 
@@ -35,7 +38,14 @@ def help() -> None:
     print("example: 1/Rules/rule/ruLe")
     print("To generate the hex board use the third command or 'Generate'")
 
-def set_hex(code: str, board: List[List[int]]) -> List[List[int]]:
+def set_hex(valid_code: str, board: List[List[int]], player: int) -> List[List[int]]:
+    splitted_code = list(valid_code)
+    row = ord(splitted_code[0]) - 65
+    col = int(splitted_code[1]) - 1
+    board[row][col] = player
+    return board
+
+def set_hex_IO(code: str, board: List[List[int]], player: int) -> List[List[int]]:
     splitted_code = list(code)
     
     if(len(splitted_code) != 2 or not splitted_code[1].isdigit()):
@@ -58,10 +68,99 @@ def set_hex(code: str, board: List[List[int]]) -> List[List[int]]:
         print(" is not a Code!")
         print(f"Position {splitted_code[0]} is not valid\n")
         return board
+    
+    row = ord(splitted_code[0]) - 65
+    col = int(splitted_code[1]) - 1
+    if(board[row][col] != 0):
+        print("")
+        print_red(code)
+        print(" is already filled!")
+        return board
+    
+    return set_hex(code, board, player)
 
-    return []
+def is_connected_recursive(i: int, j: int, player: int, board: List[List[int]], visited: List[Tuple[int, int]]) -> Tuple[bool, List[Tuple[int, int]]]:
+    if i >= len(board) or j >= len(board) or i < 0 or j < 0:
+        return False, visited
+    else:
+        visited.append((i, j))
+        if(board[i][j] == player):
+            if(player == 1 and j == len(board)-1):
+                return True, visited
+            elif(player == 2 and i == len(board)-1):
+                return True, visited
+            else:
+                neighbors = {(0, 1), (-1, 1), (-1, 0), (0, -1), (1, -1), (1, 0)}
+                for (neighbor_i, neighbor_j) in neighbors:
+                    new_i = i + neighbor_i
+                    new_j = j + neighbor_j
+                    if(not (new_i, new_j) in visited):
+                        found, visited = is_connected_recursive(new_i, new_j, player, board, visited)
+                        if found:
+                            return found, visited
+                return False, visited
 
+        else:
+            return False, visited
+
+
+def check_win_player1(board: List[List[int]]) -> bool:
+    # PLAYER 1 goal is connecting the left and right side of the board
+    
+    # simple check first
+    for i in range(len(board)):
+        if board[i][0] == 1:
+            break
+    else:
+        return False
+
+    for i in range(len(board)):
+        if board[i][len(board)-1] == 1:
+            break
+    else:
+        return False
+    
+    # deep check
+    for i in range(len(board)):
+        if board[i][0] == 1:
+            visited = []
+            result_recur = is_connected_recursive(i, 0, 1, board, visited)
+    return result_recur[0]
+
+def check_win_player2(board: List[List[int]]) -> bool:
+    # PLAYER 2 goal is connecting the top and bottom side of the board
+    
+    # simple check first
+    for i in range(len(board)):
+        if board[0][i] == 2:
+            break
+    else:
+        return False
+
+    for i in range(len(board)):
+        if board[len(board)-1][i] == 2:
+            break
+    else:
+        return False
+    
+    # deep check
+    for i in range(len(board)):
+        if board[0][i] == 2:
+            visited = []
+            result_recur = is_connected_recursive(0, i, 2, board, visited)
+    return result_recur[0]
+
+    
 if __name__ == "__main__":
-    board = [[0 for _ in range(5)] for _ in range(5)]
-    board = set_hex("F0", board)
-    menu()
+    board = [[1, 1, 0, 2, 0],
+             [0, 1, 0, 2, 2],
+             [2, 2, 2, 1, 1],
+             [2, 2, 1, 1, 0],
+             [1, 1, 2, 1, 1]]    # Assuming set_hex_IO and display_board functions are defined
+    display_board(board)
+
+    result1 = check_win_player1(board)
+    print(f"Player 1 : {result1}")
+    result2 = check_win_player2(board)
+    print(f"Player 2 : {result2}")
+
--- src/main.py
+++ src/main.py
@@ -6,86 +6,14 @@ from typing import List
 # PLAYER 1 is O with the color BLUE
 # PLAYER 2 is X with the color RED
 
-# Function to check if Player 1 has won
-def is_player1_winner(board: List[List[int]]) -> bool:
-    def dfs(row, col):
-        if col < 0 or col >= 5:
-            return False
-
-        if board[row][col] != 1:
-            return False
-
-        if visited[row][col]:
-            return False
-
-        visited[row][col] = True
-
-        if col == 4:
-            return True  # Player 1 has connected the left and right sides
-
-        for dr, dc in neighbors:
-            if dfs(row + dr, col + dc):
-                return True
-
-        return False
-
-    neighbors = [(0, 1), (0, -1), (-1, 1), (1, -1), (-1, 0), (1, 0)]
-    visited = [[False] * 5 for _ in range(5)]
-
-    # Check for a path from the left side (column 0) to the right side (column 4) for Player 1
-    for r in range(5):
-        if board[r][0] == 1 and dfs(r, 0):
-            return True
-
-    return False
-
-# Function to check if Player 2 has won
-def is_player2_winner(board: List[List[int]]) -> bool:
-    def dfs(row, col):
-        if row < 0 or row >= 5:
-            return False
-
-        if board[row][col] != 2:
-            return False
-
-        if visited[row][col]:
-            return False
-
-        visited[row][col] = True
-
-        if row == 4:
-            return True  # Player 2 has connected the top and bottom sides
-
-        for dr, dc in neighbors:
-            if dfs(row + dr, col + dc):
-                return True
-
-        return False
-
-    neighbors = [(0, 1), (0, -1), (-1, 1), (1, -1), (-1, 0), (1, 0)]
-    visited = [[False] * 5 for _ in range(5)]
-
-    # Check for a path from the top side (row 0) to the bottom side (row 4) for Player 2
-    for c in range(5):
-        if board[0][c] == 2 and dfs(0, c):
-            return True
-
-    return False
-
-
-# Function to check if any player has won
-def is_game_won(board: List[List[int]]) -> bool:
-    return is_player1_winner(board) or is_player2_winner(board)
-
-
 # Main function
 def main():
-    # Example board
     display_intro()
     command = menu()
     if(command == 1): rules()
     elif(command == 2): help()
     elif(command == 3): None
+    elif(command == 4): None
 
 if __name__ == "__main__":
     main()
